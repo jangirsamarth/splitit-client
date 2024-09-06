1da67838850fc9f4c14d9093a56e02f8f50feddc
jangirsamarth <samarth8947@gmail.com>
2024-09-06 15:59:13 +0530

    updated copyright.jsx

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -6,7 +6,6 @@ export default function Copyright() {
 
   return (
     <Typography mt="-2" variant="body2" align="center" sx={{ color: 'text.secondary'}}>
-            &copy; Sunny George | Open Source &nbsp; 
             <FavoriteIcon  color="error"  sx={{ fontSize: 18 ,mb:'-2px', ml:'-5px' }}/><br/>
             <Link variant="subtitle3" component={'a'} href="https://github.com/jangirsamarth/split-it" target="_blank">
             [GitHub]
