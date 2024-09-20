78d180b70b5e43fb753e3bf8e4e2d0c59f845999
jangirsamarth <samarth8947@gmail.com>
2024-09-20 17:24:30 +0530

    Refactor Copyright component and remove unused imports

--- src/components/Copyright.jsx
+++ src/components/Copyright.jsx
@@ -6,10 +6,7 @@ export default function Copyright() {
 
   return (
     <Typography mt="-2" variant="body2" align="center" sx={{ color: 'text.secondary'}}>
-            <FavoriteIcon  color="error"  sx={{ fontSize: 18 ,mb:'-2px', ml:'-5px' }}/><br/>
-            <Link variant="subtitle3" component={'a'} href="https://github.com/jangirsamarth/split-it" target="_blank">
-            [GitHub]
-            </Link>
+            
           </Typography>
   )
 }
